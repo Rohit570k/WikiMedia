f96fbbde5473022409da04bb0fb8aea891da82e5
ROHIT KUMAR <rk15072000@gmail.com>
2023-06-27 13:52:37 +0530

    localdb

--- app/src/main/java/com/example/wikimedia/ui/images/ImagesFragment.java
+++ app/src/main/java/com/example/wikimedia/ui/images/ImagesFragment.java
@@ -7,26 +7,77 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.wikimedia.data.models.Articles.ArticleResponse;
+import com.example.wikimedia.data.models.Articles.Pages;
 import com.example.wikimedia.databinding.FragmentImagesBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
 
 public class ImagesFragment extends Fragment {
 
+
     private FragmentImagesBinding binding;
+
+    private final static String TAG = "ImagesFragment";
+
+    private ImagesViewModel  imagesViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ImagesViewModel imagesViewModel =
-                new ViewModelProvider(this).get(ImagesViewModel.class);
+                new ViewModelProvider(this, new ViewModelProvider.Factory() {
+                    @NonNull
+                    @Override
+                    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                        return (T) new ImagesViewModel(getActivity().getApplicationContext());
+                    }
+                }).get(ImagesViewModel.class);
 
         binding = FragmentImagesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        imagesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //String  date = "2023-06-26";
+
+        // Get the current date
+        Date currentDate = new Date();
+
+        // Format the date as "yyyy-MM-dd"
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String date = dateFormat.format(currentDate);
+
+        imagesViewModel.getPageOfTheDay(date).observe(getViewLifecycleOwner(), new Observer<ArticleResponse>() {
+            @Override
+            public void onChanged(ArticleResponse response) {
+
+                Map<String, Pages> map = response.getQuery().getPages();
+                //Get first value here only value is there
+                Map.Entry<String, Pages> entry = map.entrySet().iterator().next();
+                String key = entry.getKey();
+                Pages page = entry.getValue();
+
+
+                String imageUrl = page.getThumbnail().getSource();
+                Glide.with(binding.imageView).load(imageUrl).into(binding.imageView);
+                binding.potd.setText("Picture of the Day: \n "+page.getTitle());
+            }
+        });
+
     }
 
     @Override
